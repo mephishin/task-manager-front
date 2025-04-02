e02cc699d35c6249e61c329fe7b0470ed0e27478
mephishin <epishin1anatoly@gmail.com>
2025-04-02 16:29:06 +0300

    - вынес создание задачи в шапку

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -10,8 +10,11 @@ import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import {Outlet} from "react-router-dom";
-import {Link} from "@mui/material";
+import {Link, Modal} from "@mui/material";
 import AuthService from "../../AuthService";
+import {useState} from "react";
+import Button from "@mui/material/Button";
+import {CreateTaskForm} from "../forms/CreateTaskForm";
 
 const pages = [
     {
@@ -43,9 +46,24 @@ const settings = [
 
     }
 ];
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 export const Navigation = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -91,6 +109,19 @@ export const Navigation = () => {
                             </MenuItem>
                         ))}
                     </Menu>
+                </Box>
+                <Box>
+                    <Button sx={{ backgroundColor: "white" }} onClick={handleOpen}>Create task</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <CreateTaskForm handleClose={handleClose} />
+                        </Box>
+                    </Modal>
                 </Box>
                 <Box>
                     <Tooltip title="Open settings">
--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -1,42 +1,22 @@
 import React, { useState, useEffect} from 'react'
 import Box from '@mui/material/Box';
 import {
+    Autocomplete,
     Card,
     CardContent,
     Divider,
     FormControl,
-    Link, Modal, Select,
-    Stack,
+    Link,
+    Stack, TextField,
     Typography
 } from "@mui/material";
 import {getAllProjects, getTasks, getTaskStatuses} from "../adapter/resources";
-import MenuItem from "@mui/material/MenuItem";
-import Button from "@mui/material/Button";
-import {CreateTaskForm} from "../components/forms/CreateTaskForm";
-
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
 export const TasksPage = () => {
     const [tasks, setTasks] = useState([]);
     const [taskStatuses, setStatuses] = useState([]);
     const [projects, setProjects] = useState([]);
     const [project, setProject] = useState('');
-
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
 
     useEffect(() => {
         getTaskStatuses()
@@ -55,40 +35,19 @@ export const TasksPage = () => {
         }
     }, [project]);
 
-    const handleChange = (event) => {
-        setProject(event.target.value);
-    };
-
     return (
         <Box>
-            <Box sx={{ margin: 2 }}>
-                <Button sx={{ backgroundColor: "white" }} onClick={handleOpen}>Create task</Button>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <CreateTaskForm handleClose={handleClose} />
-                    </Box>
-                </Modal>
-            </Box>
             <Box>
-                <FormControl sx={{minWidth: 120, backgroundColor: "white", borderRadius: 2, margin: 2, padding: 2}}>
-                    <Typography color="black">Choose project</Typography>
-                    <Select
+                <FormControl sx={{minWidth: "25%", backgroundColor: "white", borderRadius: 2, margin: 1, padding: 1}}>
+                    <Autocomplete
                         value={project.name}
-                        onChange={handleChange}
+                        onChange={(event, newValue) => setProject(newValue)}
+                        disablePortal
+                        options={projects.map(project => project.name)}
+                        renderInput={(params) => <TextField {...params} label="project" />}
                         displayEmpty
-                        // label="Project"
                     >
-                        {projects.map((project) => (
-                            <MenuItem value={project.name}>
-                                <Typography sx={{ textAlign: 'center' }}>{project.name}</Typography>
-                            </MenuItem>
-                        ))}
-                    </Select>
+                    </Autocomplete>
                 </FormControl>
             </Box>
             <Box>
