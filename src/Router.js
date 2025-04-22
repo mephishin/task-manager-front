efefabe6cf4889e9b9a939a0de2217d48573dede
mephishin <epishin1anatoly@gmail.com>
2025-04-22 22:25:27 +0300

    - внес изменения в навигацию

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {Navigation} from "./components/navigation/Navigation";
 import {NoPage} from "./pages/NoPage";
 import {TaskPage} from "./pages/TaskPage";
@@ -8,7 +8,8 @@ export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigation/>}>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Navigate to={"/project"} />} />
                     <Route path="/project" element={<ProjectPage />} />
                     <Route path="/project/:project" element={<ProjectPage />} />
                     <Route path="/task/:key" element={<TaskPage />} />
